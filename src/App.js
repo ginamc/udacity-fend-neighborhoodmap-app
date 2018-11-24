99d366d291379b3275cf71e55c78793a62c45ff2
ginamc <gm.cairney@gmail.com>
2018-11-23 20:58:59 -0500

    fix up variable names
    clean code up for better tracking

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import axios from 'axios'
 // axios downloaded as per Yahya Elharony's walkthrough -- specified in README
 // axios used to work with Foursquare API
 
+// import MapDisplay from './components/MapDisplay'
+
 import './App.css';
 
 class App extends Component {
 
-    //set the state for our food places and markers
+    //set the state for our food places and venuePins
     state = {
-        foodPlaces: [],
-        markers: []
+        venueInfo: [],
+        venuePins: []
     }
 
     
@@ -19,45 +21,45 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            query: ''
+            query: '',
         }
     }
 
     //call our rendered rendered map
     // and invoke a function to get our food info
     componentDidMount() {
-        this.getPlaces()
+        this.getFsqPlaces()
     }
 
     // function loads the map with my api
     // initializes initMap so the app can read and do the callback in the url
-    loadMap = () => {
-        loadMapScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyC0QW_gzDqHmmc48BjJrgRPRkOEi_FD4HM&callback=initMap")
+    loadFoodMap = () => {
+        loadFoodMapScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyC0QW_gzDqHmmc48BjJrgRPRkOEi_FD4HM&callback=initMap")
         window.initMap = this.initMap
     }
 
     // retrieve places information from Foursquare
     // with the list of objects we want to show
-    getPlaces = () => {
+    getFsqPlaces = () => {
         let endPoint = "https://api.foursquare.com/v2/venues/explore?"
         let parameters = {
             client_id: "J4H31X3T25IDXZF3IMI3A12WNTUDKZ43MGIWNGYJLFGJFLH4",
             client_secret: "KPDF2NV5SL4UKISN24J1KJF13Q1F20WXKYURK2I1LJ5YIYCQ",
             query: "food",
-            near: "Bushwick, Brooklyn, NY",
+            near: "Bushwick, NY",
             v: "20182507"
         }
 
         // use axios to get Foursquare data into our app
-        // use the endPoint url and our parameter objects to display desired info over the markers
+        // use the endPoint url and our parameter objects to display desired info over the venuePins
         // when we get our info, log it
-        // call the state from above, pass the info into 'foodPlaces' save the info, then render the map
+        // call the state from above, pass the info into 'venueInfo' save the info, then render the map
         // catch any errors
         axios.get(endPoint + new URLSearchParams(parameters))
             .then(response => {
                 this.setState({
-                    foodPlaces: response.data.response.groups[0].items
-                }, this.loadMap())
+                    venueInfo: response.data.response.groups[0].items
+                }, this.loadFoodMap())
             })
             .catch(error => {
                 console.log("ERROR! " + error)
@@ -66,29 +68,29 @@ class App extends Component {
 
     // create the map!
     // display our infomation for a selected marker
-    // loop over our state (foodPlaces) to populate markers onto the map
-    // pass the venue info into the infowindow variable and
+    // loop over our state (venueInfo) to populate venuePins onto the map
+    // pass the venue info into the venuePopup variable and
     // for each place, dynamically create a marker
     initMap = () => {
-        let map = new window.google.maps.Map(document.getElementById('map'), {
+        let venueMap = new window.google.maps.Map(document.getElementById('map'), {
             center: { lat: 40.703177, lng: -73.923904 },
             zoom: 15
         });
 
-        let infowindow = new window.google.maps.InfoWindow()
-        this.state.foodPlaces.map(foodVenue => {
-            let contentString = `${foodVenue.venue.name}`
-            let marker = new window.google.maps.Marker({
-                position: { lat: foodVenue.venue.location.lat, lng: foodVenue.venue.location.lng },
-                map: map,
-                name: foodVenue.venue.name
+        let venuePopup = new window.google.maps.InfoWindow()
+        this.state.venueInfo.map(loadVenueInfo => {
+            let popupContents = `${loadVenueInfo.venue.name}`   // <-- change what goes into this variable 
+            let locationMarker = new window.google.maps.Marker({
+                position: { lat: loadVenueInfo.venue.location.lat, lng: loadVenueInfo.venue.location.lng },
+                map: venueMap,
+                name: loadVenueInfo.venue.name
             });
 
             // tie everything together in an event listener
-            // change the content, and on click, open the infowindow with relevant info
-            marker.addListener('click', function() {
-                infowindow.setContent(contentString)
-                infowindow.open(map, marker);
+            // change the content, and on click, open the venuePopup with relevant info
+            locationMarker.addListener('click', function() {
+                venuePopup.setContent(popupContents)
+                venuePopup.open(venueMap, locationMarker);
             });
         });
     }
@@ -96,12 +98,11 @@ class App extends Component {
     
     // loop thru each marker and check that the query matches our input in search bar
     filterFood(query) {
-      
-     this.markers.forEach(marker => {
-            console.log(marker);
-            marker.name.toLowerCase().includes(query.toLowerCase()) === true ?
-                marker.setVisible(true) :
-                marker.setVisible(false)
+      this.state.venuePins.forEach(locationMarker => {
+            console.log(locationMarker);
+            locationMarker.name.toLowerCase().includes(query.toLowerCase()) === true ?
+                locationMarker.setVisible(true) :
+                locationMarker.setVisible(false)
         });
 
         this.setState({ query });
@@ -117,8 +118,8 @@ class App extends Component {
             <input value = { this.state.query }
             onChange = {
                 (e) => {this.filterFood(e.target.value)}}/> <br/> 
-                { this.state.foodVenue && this.state.foodVenue.length > 0 && this.state.foodVenue.map((foodVenue, index) => ( 
-                  <div className = "foodplace-item"> { foodVenue.name } </div>
+                { this.state.loadVenueInfo && this.state.loadVenueInfo.length > 0 && this.state.loadVenueInfo.map((loadVenueInfo, index) => ( 
+                  <div className = "foodplace-item"> { loadVenueInfo.name } </div>
                 ))
             }
             </div> 
@@ -133,7 +134,7 @@ class App extends Component {
 // and create the script element (i.e. <script></script>)
 // url is called in the function
 // insert the script at the vergy beginning of our script and pass it down to children
-function loadMapScript(url) {
+function loadFoodMapScript(url) {
     let index = window.document.getElementsByTagName("script")[0]
     let script = window.document.createElement("script")
 
