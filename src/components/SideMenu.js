c1244ac729f0819f479df37dcc571125726c7ff4
ginamc <gm.cairney@gmail.com>
2018-11-26 23:35:41 -0500

    commit

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 
 class SideMenu extends Component {
-
+    styles = {
+        
+    }
 
     render = () => {
         return (
-
+            console.log("There be a sidemen here")
         )
     }
 }
