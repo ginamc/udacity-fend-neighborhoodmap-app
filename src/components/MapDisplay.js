e3ea2536de597c0b58d4214ff1f5ddcf13452620
ginamc <gm.cairney@gmail.com>
2018-11-27 19:01:07 -0500

    Add aria role to map

--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react'
 
 
 class MapDisplay extends Component {
-
+render() {
+    return (
+        <main>
+            <div role="application" aria-hidden="true" id="map"></div>
+        </main>
+    );
+}
 
 }
 
